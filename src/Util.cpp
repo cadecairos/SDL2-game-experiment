d97b0d25717d5ce869c341beb2458e96764d506a
Christopher De Cairos <cade@mozillafoundation.org>
2016-05-18 08:21:05 -0400

    GPLv3

--- src/Util.cpp
+++ src/Util.cpp
@@ -1,3 +1,20 @@
+/*
+** Copyright 2016 Christopher De Cairos
+**
+** This file is part of Ghost Hunter.
+**
+** Ghost Hunter is free software: you can redistribute it and/or modify it under
+** the terms of the GNU General Public License as published by the Free Software
+** Foundation, either version 3 of the License, or (at your option) any later version.
+**
+** Ghost Hunter is distributed in the hope that it will be useful, but WITHOUT ANY 
+** WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+** PARTICULAR PURPOSE. See the GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License along with
+** Ghost Hunter. If not, see http://www.gnu.org/licenses/.
+*/
+
 #include "Util.h"
 
 SDL_Surface *load_image(std::string filename) {
